e951c702fa0731d9dea75a5586379df919bb1bb5
LeonardJung <92081731+LeonardJung@users.noreply.github.com>
2023-03-07 12:56:02 +0100

    giving Delete icon functionality

--- KalenderApp/src/App.jsx
+++ KalenderApp/src/App.jsx
@@ -21,10 +21,16 @@ function App() {
       day: "March 9.",
     },
   ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="App">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
