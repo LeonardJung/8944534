13a061c8f38e8c5f32be91cb3ed29c27ac1dd4c8
LeonardJung <92081731+LeonardJung@users.noreply.github.com>
2023-03-07 12:26:01 +0100

    Put tasks at top level

--- KalenderApp/src/App.jsx
+++ KalenderApp/src/App.jsx
@@ -4,10 +4,27 @@ import "./App.css";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Task 1",
+      day: "March 7.",
+    },
+    {
+      id: 2,
+      text: "Task 2",
+      day: "March 8.",
+    },
+    {
+      id: 3,
+      text: "Task 3",
+      day: "March 9.",
+    },
+  ]);
   return (
     <div className="App">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- KalenderApp/src/components/Tasks.jsx
+++ KalenderApp/src/components/Tasks.jsx
@@ -1,25 +1,6 @@
 import React from "react";
-import { useState } from "react";
 
-const tasks = [
-  {
-    id: 1,
-    text: "Task 1",
-    day: "March 7.",
-  },
-  {
-    id: 2,
-    text: "Task 2",
-    day: "March 8.",
-  },
-  {
-    id: 3,
-    text: "Task 3",
-    day: "March 9.",
-  },
-];
-
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
