dacb32f83bc67e012b73af054f3ef7b62734cf1c
LeonardJung <92081731+LeonardJung@users.noreply.github.com>
2023-03-07 12:04:24 +0100

    Building Button Component

---
+++ KalenderApp/src/components/DayPage/Button.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+  text: "Button",
+};
+
+export default Button;
